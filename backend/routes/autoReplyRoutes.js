0ada38a9ede6b9e9b95bdca790d9ccc556c9d8b6
prashant272 <prashantkrjha12@gmail.com>
2026-04-26 20:58:08 +0530

    fix auto replay error

--- backend/routes/autoReplyRoutes.js
+++ backend/routes/autoReplyRoutes.js
@@ -5,13 +5,13 @@ import {
   updateAutoReply, 
   deleteAutoReply 
 } from "../controllers/autoReplyController.js";
-import { protect, adminOnly } from "../middleware/authMiddleware.js";
+import { protect, restrictTo } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", protect, getAutoReplies);
-router.post("/", protect, adminOnly, createAutoReply);
-router.put("/:id", protect, adminOnly, updateAutoReply);
-router.delete("/:id", protect, adminOnly, deleteAutoReply);
+router.post("/", protect, restrictTo("Admin"), createAutoReply);
+router.put("/:id", protect, restrictTo("Admin"), updateAutoReply);
+router.delete("/:id", protect, restrictTo("Admin"), deleteAutoReply);
 
 export default router;
